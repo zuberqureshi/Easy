6533f6f3899ffc51aa35ed2b86e8fe95ab9a0fe9
zuber qureshi <zuberqureshi95@gmail.com>
2023-06-17 19:37:31 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+ 
+import React from 'react';
+import {  Text, View } from 'react-native';
+
+
+
+function App() {
+ 
+ 
+  return (
+
+            <View > 
+              <Text > Hi </Text>
+              
+            </View>
+        
+  );
+}
+
+ 
+
+export default App;
