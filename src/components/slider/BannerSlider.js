497363b91e09617c1b8682258a387e69bfbc9921
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2023-06-22 15:19:45 +0530

    banner done (#11)

--- src/components/slider/BannerSlider.js
+++ src/components/slider/BannerSlider.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {View, Image} from 'react-native';
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from "react-native-responsive-dimensions";
 export default function BannerSlider({data}) {
+
+  console.log("silder data",data)
   return (
     <View>
       <Image
-        source={data.image}
+        source={{uri:data.item.url}}
         style={{height: responsiveHeight(20), width: responsiveWidth(85), borderRadius: responsiveWidth(2.5)}}
       />
     </View>
